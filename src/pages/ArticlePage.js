fe68a05ec7d5503a3ca866100ca55b6e8426a58f
jimdcodes <jdinhcodes@gmail.com>
2023-06-30 15:31:39 -0700

    Adding article-content to articlepage

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,13 +1,22 @@
 import { useParams } from 'react-router-dom';
+import articles from './article-content';
 
 const ArticlePage = () => {
     // const params = useParams();
     // const articleId = params.articleId;
     // const { articleId } = params;
     const { articleId } = useParams();
+    const article = articles.find(article => article.name === articleId);
 
     return (
+        // Using react Fragment, <></>, since you cannot return more than one top level component
+        <>
         <h1>This is the article page for the article with id: {articleId}!</h1>
+        <h1>{article.title}</h1>
+        {article.content.map(paragraph => (
+            <p>{paragraph}</p>
+        ))}
+        </>
     );
 }
 
