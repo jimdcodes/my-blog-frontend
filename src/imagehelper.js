3afe4e67f20e9370cf6283e6c5efe4eaee5034e2
jimdcodes <jdinhcodes@gmail.com>
2023-07-01 18:07:34 -0700

    Fixing words

--- src/imagehelper.js
+++ src/imagehelper.js
@@ -4,7 +4,6 @@ const imageList = images.keys().map(image => images(image));
 function ImageGallery() {
   return (
     <div>
-      <h1>This is the image page!</h1>
       {imageList.map((image, index) => (
         <img key={index} src={image} alt={`image-${index}`} />
       ))}
