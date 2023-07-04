d8669146cb06d071e0ff6816c5448a638a5757b7
jimdcodes <jdinhcodes@gmail.com>
2023-07-04 15:55:21 -0700

    Added notfoundpage to URL after /articles/

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router-dom';
 import articles from './article-content';
+import NotFoundPage from './NotFoundPage';
 
 const ArticlePage = () => {
     // const params = useParams();
@@ -7,6 +8,10 @@ const ArticlePage = () => {
     // const { articleId } = params;
     const { articleId } = useParams();
     const article = articles.find(article => article.name === articleId);
+
+    if (!article) {
+        return <NotFoundPage />
+    }
 
     return (
         // Using react Fragment, <></>, since you cannot return more than one top level component
