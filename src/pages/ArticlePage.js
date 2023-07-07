a44d10fb3c6107390c2e099da4c6bd096fc2bd34
jimdcodes <jdinhcodes@gmail.com>
2023-07-07 11:44:08 -0700

    using useEffect and useState

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,8 +1,15 @@
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import articles from './article-content';
 import NotFoundPage from './NotFoundPage';
 
 const ArticlePage = () => {
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+    useEffect(() => {
+        setArticleInfo({ upvotes: 3, comments: [] });
+    });
+
     // const params = useParams();
     // const articleId = params.articleId;
     // const { articleId } = params;
@@ -17,6 +24,7 @@ const ArticlePage = () => {
         // Using react Fragment, <></>, since you cannot return more than one top level component
         <>
         <h1 key={article.title}>{article.title}</h1>
+        <p>This article has {articleInfo.upvotes} upvote(s)</p>
         <p key={article.content}>{article.content}</p>
         {/* {article.content.map(paragraph => (
             <p>{paragraph}</p>
