ddca1979b502cd8da07c75b030a0a5c053987add
jimdcodes <jdinhcodes@gmail.com>
2023-07-14 20:41:49 -0700

    Layout to comment section

---
+++ src/components/AddCommentForm.js
@@ -0,0 +1,18 @@
+const AddCommentForm = () => {
+    return (
+        <div id="add-comment-form">
+            <h3>Add a Comment</h3>
+            <label>
+                Name:
+                <input type="text" />
+            </label>
+            <label>
+                Comment:
+                <textarea rows="4" cols="50" />
+            </label>
+            <button>Add Comment</button>
+        </div>
+    )
+}
+
+export default AddCommentForm;
