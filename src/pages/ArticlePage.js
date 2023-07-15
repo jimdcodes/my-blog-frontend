cd460d1f335789e08e0de77ea7121e18743af7b2
jimdcodes <jdinhcodes@gmail.com>
2023-07-14 19:41:28 -0700

    Fixing error

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -30,8 +30,8 @@ const ArticlePage = () => {
         // Using react Fragment, <></>, since you cannot return more than one top level component
         <>
         <h1 key={article.title}>{article.title}</h1>
-        <p>This article has {articleInfo.upvotes} upvote(s)</p>
-        <p key={article.content}>{article.content}</p>
+        <p key={article.title + ": " + article.articleInfo}>This article has {articleInfo.upvotes} upvote(s)</p>
+        <p key={article.content + article.title}>{article.content}</p>
         {/* {article.content.map(paragraph => (
             <p>{paragraph}</p>
         ))} */}
