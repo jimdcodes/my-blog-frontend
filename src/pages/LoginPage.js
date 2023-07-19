db0c67ca44b88078565cf3da8c1baec940262e02
jimdcodes <jdinhcodes@gmail.com>
2023-07-18 19:46:09 -0700

    Adding useStates for Login

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,4 +1,10 @@
+import { useState } from 'react';
+
 const LoginPage = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
     return (
         <h1>Log In</h1>
     );
