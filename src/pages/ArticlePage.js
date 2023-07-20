8ed77f36074e3dfc5a5c815d2c8cea6d8257f75b
jimdcodes <jdinhcodes@gmail.com>
2023-07-19 21:07:36 -0700

    Adding headers token throughout Upvote,Comment

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -30,7 +30,9 @@ const ArticlePage = () => {
     const article = articles.find(article => article.name === articleId);
 
     const addUpvote = async () => {
-        const response = await axios.put(`/api/articles/${articleId}/upvote`);
+        const token = user && await user.getIdToken();
+        const headers = token ? { authtoken: token } : {};
+        const response = await axios.put(`/api/articles/${articleId}/upvote`, null, { headers });
         const updatedArticle = response.data;
         setArticleInfo(updatedArticle);
     }
