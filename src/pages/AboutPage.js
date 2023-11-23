6169e898a895ff4b0973c8ab5b67016d4aec0564
jimdcodes <jdinhcodes@gmail.com>
2023-11-22 17:41:02 -0800

    Editting about and home page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -3,20 +3,26 @@ const AboutPage = () => {
         <>
     <h1>About Me</h1>
     <p>
-        Welcome to my page! I'm Jimmy Dinh, a dedicated professional with a passion for leveraging technology to drive impactful results. With a Bachelor of Science degree in Physics from California State University of Los Angeles, I have cultivated a diverse skill set in various technical and analytical tools, including SQL, Power BI, Python, and Mathematica. My expertise extends to Agile methodologies, Scrum, and a range of programming languages, such as JavaScript, React, HTML, and 
+        Welcome to my page! I'm Jimmy Dinh, a dedicated professional with a passion for leveraging technology to drive impactful results. With a Bachelor of Science degree in Physics from California State University of Los Angeles, I have cultivated a diverse skill set in various technical and analytical tools, including SQL, Power BI, Python, and Mathematica. My expertise extends to Agile & Scrum methodologies, and a range of programming languages, such as JavaScript, React, HTML, and 
         CSS.
     </p>
     <p>
-        My previous experiences in roles such as an IT Data Support Specialist and a Laboratory Administrator have equipped me with strong organizational and communication skills, alongside a commitment to maintaining confidentiality and creating an environment conducive to learning and growth. I have also had the privilege of serving as an Instructional Student Tutor and a Supplemental Instructor, allowing me to contribute to academic improvements in STEM subjects through personalized instruction and innovative teaching 
+        My previous experiences in roles equipped me with strong organizational and communication skills, alongside a commitment to maintaining confidentiality and creating an environment conducive to learning and growth. I have also had the privilege of serving as an Instructional Student Tutor and a Supplemental Instructor, allowing me to contribute to academic improvements in STEM subjects through personalized instruction and innovative teaching 
         strategies.
     </p>
     <p>
-        Driven by a proactive and adaptable approach, I constantly seek to expand my horizons. My passion for technology is reflected in my projects, including the development of a blog website and an Android app that tracks live COVID-19 information. Additionally, my completion of various HTML and CSS projects underscores my commitment to continuous learning and 
+        Driven by a proactive and adaptable approach, I constantly seek to expand my horizons. My passion for technology is reflected in my projects, including the development of a blog website and an Android/Web app that used to track live COVID-19 information. Additionally, my completion of various HTML and CSS projects underscores my commitment to continuous learning and 
         development.
     </p>
     <p>
-    I am enthusiastic about utilizing my skills and experience to contribute to meaningful projects and initiatives. Feel free to explore my work and reach out for collaboration or any 
-    inquiries.
+        I am enthusiastic about utilizing my skills and experience to contribute to meaningful projects and initiatives. Feel free to explore my work and reach out for collaboration or any 
+        inquiries.
+    </p>
+    <p>
+        You can reach out to me via
+        <b> <a href="mailto:jdinh497@gmail.com">Gmail</a> </b>
+        or 
+        <b> <a href="https://www.linkedin.com/in/jdinh5/">LinkedIn</a></b>
     </p>
     </>
     );
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,25 +2,17 @@ const HomePage = () =>(
     <>
     <h1>Welcome to my blog!</h1>
     <p>
-        I'm Jimmy Dinh, a dynamic professional with a background in Physics and a fervent passion for technology and data. This blog serves as a space where I share insights, experiences, and knowledge gained from my diverse professional 
-        journey.
+        I'm Jimmy Dinh, a dynamic professional with a background in Physics and a passion for technology and data. This blog serves as a space where I share my experiences, projects, and other random 
+        things!
     </p>
     <p>
-        Having worked in roles ranging from IT Data Support Specialist to Laboratory Administrator, I've accumulated a wealth of experience in data management, analysis, and system support. With a strong foundation in various technical tools such as SQL, Python, and Power BI, I bring a unique perspective to the intersection of science and 
-        technology.
+        I love working with computers! I am a big fan of reading about computer parts (mainly GPUs) and browsing reddit. My hobbies includes learning to code, gaming, excerising, and cooking with ingredients around the house.
     </p>
     <p>
-        Through my posts, I aim to offer valuable tips, techniques, and best practices, covering topics such as data analysis, programming, and project management. I also delve into my journey of continuous learning, sharing my discoveries and challenges as I navigate the ever-evolving landscape of 
-        technology.
-    </p>
-    <p>
-        Join me on this enriching voyage of exploration and learning. Together, let's uncover the endless possibilities that technology and data hold in shaping our 
-        future.
-    </p>
-    <p>
-        Feel free to explore my posts, and don't hesitate to reach out if you have any questions, suggestions, or collaboration 
+        Join me on this enriching voyage of exploration and learning. Don't hesitate to reach out if you have any questions, suggestions, or collaboration 
         opportunities.
     </p>
+    <br/>
     <p>
         Happy reading everyone!
     </p>
